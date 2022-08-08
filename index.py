f8bae4d83e0210e983dc04ad7eae5b0030c14927
roywatermelon11370 <53699625+roywatermelon11370@users.noreply.github.com>
2022-08-08 22:10:59 +0800

    Added some themes

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,request,redirect,url_for,flash
 app=Flask(__name__)
 notelst=[]
 noteTem={'id':'','ttle':'','conten':'','tgs':[]}
-settings={'rmAnim' : False, 'darkMode' : False}
+setts={'rmAnim' : False, 'theme':'light'}
 
 
 def maxIdPlusOne():
@@ -24,12 +24,12 @@ def findItemById(x):
 
 @app.route("/")
 def index():
-    return render_template('index.html', notelst=notelst)
+    return render_template('index.html', notelst=notelst, setts=setts)
 
 
 @app.route("/add")
 def add():
-    return render_template('add.html')
+    return render_template('add.html', setts=setts)
 
 
 @app.route("/addAction", methods=['POST'])
@@ -43,13 +43,13 @@ def addAction():
     print(conten)
     print(tgs)
     notelst.append({'id':id,'title':ttle,'content':conten,'tags':tgs})
-    return redirect(url_for('index'))
+    return redirect('/')
 
 
 @app.route("/detail/<int:addId>", methods=['GET'])
 def ShowDetail(addId):
     curr=findItemById(addId)
-    return render_template('detail.html',noteitem=curr)
+    return render_template('detail.html',noteitem=curr, setts=setts)
 
 
 @app.route("/edit/<int:editId>", methods=['GET'])
@@ -59,7 +59,7 @@ def edit(editId):
         tagsShow=','.join(list(curr['tags']))
     else:
         tagsShow=0
-    return render_template('edit.html',noteitem=curr,tags=tagsShow)
+    return render_template('edit.html',noteitem=curr,tags=tagsShow, setts=setts)
 
 
 @app.route("/editAction", methods=['POST'])
@@ -77,7 +77,7 @@ def editAction():
 
 @app.route("/manage")
 def manage():
-    return render_template('manage.html', notelst=notelst)
+    return render_template('manage.html', notelst=notelst, setts=setts)
 
 
 @app.route("/manageAction", methods=['POST'])
@@ -98,7 +98,14 @@ def delete(delId):
 
 @app.route("/settings")
 def settings():
-    return render_template('settings.html')
+    return render_template('settings.html', setts=setts)
+
+
+@app.route("/settingsSave",methods=['POST'])
+def settSave():
+    theme=request.form['theme']
+    setts.update({'theme': theme})
+    return redirect('/')
 
 
 @app.route("/search")
@@ -110,8 +117,8 @@ def search():
             for j in i['tags']:
                 if j==keyWord:
                     results.append(i)
-        return render_template('search.html',resultlst=results,keyword=keyWord)
-    return  render_template('search.html',keyword=False,resultlst=False)
+        return render_template('search.html',resultlst=results,keyword=keyWord,setts=setts)
+    return  render_template('search.html',keyword=False,resultlst=False, setts=setts)
  
 
 if __name__=='__main__':
